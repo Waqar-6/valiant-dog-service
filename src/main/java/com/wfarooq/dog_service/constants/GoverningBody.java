bd1099879d02174a94d4c1aded56490ddc020d46
Waqar <waqar-1@hotmail.co.uk>
2025-03-25 23:17:03 +0000

    feature: add dog entity and enums for dogbreed and governingbody

---
+++ src/main/java/com/wfarooq/dog_service/constants/GoverningBody.java
@@ -0,0 +1,18 @@
+package com.wfarooq.dog_service.constants;
+
+public enum GoverningBody {
+    ADRK,           // Allgemeiner Deutscher Rottweiler-Klub (Germany)
+    AKC,            // American Kennel Club (USA)
+    FCI,            // Fédération Cynologique Internationale (International)
+    UKC,            // United Kennel Club (USA)
+    KC,             // The Kennel Club (UK)
+    CKC,            // Canadian Kennel Club
+    ANKC,           // Australian National Kennel Council
+    NZKC,           // New Zealand Kennel Club
+    NAPR,           // North American Purebred Registry
+    IOEBA,          // International Olde English Bulldogge Association
+    ABKC,           // American Bully Kennel Club
+    USBR,           // United States Bully Registry
+    EBKC,           // European Bully Kennel Club
+    UCA             // United Canine Association
+}
