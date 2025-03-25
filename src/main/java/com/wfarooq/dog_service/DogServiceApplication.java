3c54bfc2eb82f0a1c2ffe207d05f9db4e8dc5de6
Waqar <waqar-1@hotmail.co.uk>
2025-03-25 22:53:49 +0000

    first commit for dog service

---
+++ src/main/java/com/wfarooq/dog_service/DogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wfarooq.dog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wfarooq/dog_service/DogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfarooq.dog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
